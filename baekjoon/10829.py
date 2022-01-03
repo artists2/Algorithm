bd63b27964702e14287b1eb8811a2e6210ffb6d1
r00t0k <artists2@naver.com>
2022-01-03 17:54:23 +0900

    2022-01-03 update

--- baekjoon/10829.py
+++ baekjoon/10829.py
@@ -1,11 +1,10 @@
 # 이진수 변환 재귀
 
 def binary(N: int):
-    bin_string: str = ''
     if N == 1:
-        return "1" + bin_string
+        return "1" 
     if N == 0:
-        return "0" + bin_string
+        return "0" 
     else:
         return binary(N//2) + str(N % 2)
 
--- baekjoon/17478.py
+++ baekjoon/17478.py
@@ -17,3 +17,4 @@ def recur(M):
 N = int(input())
 print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
 recur(N)
+
