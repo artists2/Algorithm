10a9db7a4147e3ab72a43f91aefdca7fe8cfaf9a
r00t0k <artists2@naver.com>
2022-03-11 00:50:50 +0900

    2022-03-11

--- baekjoon/1012.py
+++ baekjoon/1012.py
@@ -1,14 +1,29 @@
+import sys
+sys.setrecursionlimit(10000)
+
+
 T = int(input())
 
+dx = [-1, 1, 0, 0]  # 좌 우
+dy = [0, 0, -1, 1]  # 하 상
+
+result = []
+
 def dfs(x, y):
-    if x < 0 or x >=M or y <0 or y >= N:
-        False
+    if x < 0 or x >= N or y <0 or y >= M:
+        return False
     if graph[x][y]:
-        pass
+        graph[x][y] = 0  # 방문체크
+        for i in range(4):
+            cx = x + dx[i]
+            cy = y + dy[i]
+            dfs(cx, cy)
+        return True
+    return False
+        
 
 
-
-
+count = 0
 
 for _ in range(T):
     M, N, K = map(int, input().split())
@@ -19,8 +34,17 @@ for _ in range(T):
         b, a = map(int, input().split())
         graph[a][b] = 1
 
-    print(M, N, K)
-    for i in graph:
-        print(i)
+    for n in range(N):
+        for m in range(M):
+            if dfs(n, m):  # 덩어리 하나 발견 할 때 마다 
+                count += 1
+    result.append(count)
+    count = 0
 
+    # print(M, N, K)
+    # for i in graph:
+    #     print(i)
+
+for r in result:
+    print(r)
 
